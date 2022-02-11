17188ee00f2acf1de9cbc5582ef83a50e2f8ce0e
deka4core <borodaenglish@gmail.com>
2022-02-11 18:23:16 +0300

    /load_photo Загрузка файла

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request
+from flask import Flask, url_for, request, redirect
 
 app = Flask(__name__)
 
@@ -224,6 +224,43 @@ def results_page(nickname, level: int, rating: float):
                                    f'составляет {rating}!', 'Желаем удачи!'])}
                      </body>
                    </html>'''
+
+
+@app.route('/load_photo', methods=['POST', 'GET'])
+def load_photo():
+    if request.method == 'GET':
+        return f'''<!doctype html>
+                                <html lang="en">
+                                  <head>
+                                    <meta charset="utf-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                    <link rel="stylesheet"
+                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                                    crossorigin="anonymous">
+                                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                                    <title>Отбор астронавтов</title>
+                                  </head>
+                                  <body>
+                                    <div><h1>Анкета претендента<br><h4>на участие в миссии</h4></h1></div>
+                                    <div>
+                                        <form class="login_form" method="post" enctype="multipart/form-data">
+                                               <div class="form-group">
+                                                    <label for="photo">Выберите файл</label><br>
+                                                    <input type="file" class="form-control-file" id="photo" name="file">
+                                                </div><br>
+                                                <img src="{url_for('static', filename='img/loaded_image.png')}"
+                                                     alt="здесь должна была быть картинка, но не нашлась"><br>
+                                                <button type="submit" class="btn btn-primary">Отправить</button>
+                                        </form>
+                                    </div>
+                                  </body>
+                                </html>'''
+    elif request.method == 'POST':
+        f = request.files['file']
+        with open(url_for('static', filename='img/loaded_image.png')[1:], 'wb') as image:
+            image.write(f.read())
+        return redirect(url_for('load_photo'))
 
 
 if __name__ == '__main__':
