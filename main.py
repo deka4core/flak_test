0430bd4a3185a0920e4a7f43abaafabe371b006c
deka4core <borodaenglish@gmail.com>
2022-02-11 17:56:20 +0300

    /results/<nickname>/<int:level>/<float:rating> Результат отбора

--- main.py
+++ main.py
@@ -203,5 +203,28 @@ def planets_choice(planet_name):
                    </html>'''
 
 
+@app.route("/results/<nickname>/<int:level>/<float:rating>")
+def results_page(nickname, level: int, rating: float):
+    return f'''<!doctype html>
+                   <html lang="en">
+                     <head>
+                       <meta charset="utf-8">
+                       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                       <link rel="stylesheet" 
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                       crossorigin="anonymous">
+                       <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                       <title>Результаты!</title>
+                     </head>
+                     <body>
+                       <h1>Результаты отбора!</h1><br>
+                       <h4>Претендента {nickname} на участие в миссии Akuranpu:</h4><br>
+                       {promotion([f'Ваш рейтинг после {level} этапа отбора',
+                                   f'составляет {rating}!', 'Желаем удачи!'])}
+                     </body>
+                   </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
