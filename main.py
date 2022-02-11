9dec13590f6bf39c8f02396f4ef849eb10167ead
deka4core <borodaenglish@gmail.com>
2022-02-11 19:07:35 +0300

    /carousel Пейзажи Марса

--- main.py
+++ main.py
@@ -263,5 +263,47 @@ def load_photo():
         return redirect(url_for('load_photo'))
 
 
+@app.route('/carousel')
+def carousel_mars():
+    return f'''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Пейзажи Марса</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+     <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+</head>
+<body><h1>Пейзажи Марса</h1>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+            <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="{url_for('static', filename='img/mars1.png')}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{url_for('static', filename='img/mars2.png')}" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="{url_for('static', filename='img/mars3.png')}" class="d-block w-100" alt="...">
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"
+     integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU"
+      crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js"
+     integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj"
+      crossorigin="anonymous"></script>
+    </body>
+    </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
