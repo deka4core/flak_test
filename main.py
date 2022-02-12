7e5eeda0f18aa086830209aa4aedc23c4bd25f01
deka4core <borodaenglish@gmail.com>
2022-02-12 15:17:29 +0300

    list_prof/<list>

--- main.py
+++ main.py
@@ -15,5 +15,10 @@ def training(prof):
     return render_template('training.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def prof_list(list):
+    return render_template('prof_list.html', list=list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
