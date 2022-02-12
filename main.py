eb3c5ce553abe2c4a0019147d52d369b29432ff4
deka4core <borodaenglish@gmail.com>
2022-02-12 14:55:11 +0300

    /training/<prof>

--- main.py
+++ main.py
@@ -9,5 +9,11 @@ def base(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    prof = prof.lower()
+    return render_template('training.html', prof=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
